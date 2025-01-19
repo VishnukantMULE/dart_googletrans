c49ff5b0613e21180334b2c907c242fb965e4cb8
vishnukantMUle <vishnukantmule@gmail.com>
2025-01-19 21:10:45 +0530

    Added all Popular Language Code

---
+++ lib/src/constants/languagecodes.dart
@@ -0,0 +1,24 @@
+class LanguageCodes {
+  static const Map<String, String> languages = {
+                                'en': 'English',
+                        'zh': 'Chinese', 'es': 'Spanish',
+                    'hi': 'Hindi',   'ar': 'Arabic',   'fr': 'French',
+                'ja': 'Japanese',    'ko': 'Korean',   'de': 'German',
+            'bn': 'Bengali',     'ta': 'Tamil',      'te': 'Telugu',
+        'mr': 'Marathi',     'gu': 'Gujarati',   'kn': 'Kannada',
+    'ml': 'Malayalam',  'pa': 'Punjabi',    'ur': 'Urdu',      'th': 'Thai',
+        'vi': 'Vietnamese', 'id': 'Indonesian', 'ms': 'Malay',
+            'ru': 'Russian',    'uk': 'Ukrainian',  'pl': 'Polish',
+                'nl': 'Dutch',      'sv': 'Swedish',    'fi': 'Finnish',
+                    'da': 'Danish',     'cs': 'Czech',      'el': 'Greek',
+                        'ro': 'Romanian',   'hu': 'Hungarian',
+                            'bg': 'Bulgarian', 'sr': 'Serbian',
+                                'hr': 'Croatian', 'sk': 'Slovak',
+                            'lt': 'Lithuanian', 'lv': 'Latvian',
+                        'et': 'Estonian',   'sl': 'Slovenian',
+                    'mt': 'Maltese',    'is': 'Icelandic',
+                'sq': 'Albanian',    'bs': 'Bosnian',
+            'mk': 'Macedonian', 'km': 'Khmer',
+        'he': 'Hebrew',     'la': 'Latin'
+  };
+}
